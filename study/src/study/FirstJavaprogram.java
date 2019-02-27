09517c010ffe3fb1580be371e4a1af5ec055f805
yoyo <yoyo@192.168.219.170>
2019-02-28 03:53:04 +0900

    ㅗ

--- study/src/study/FirstJavaprogram.java
+++ study/src/study/FirstJavaprogram.java
@@ -4,7 +4,7 @@ public class FirstJavaprogram {
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to Java");
-		System.out.println("mmmmmmmmmmmmmmmmFirst Java Programmmmmmmm");
+		System.out.println("mFirst Java Programmmmmmmm");
 
 	}
 
